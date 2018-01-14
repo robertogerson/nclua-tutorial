378762c0918a20790d53f29a2092051f97981a87
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2018-01-14 14:34:24 +0100

    exemplo_01: Force label='' for lambda events

--- exemplos/exemplo_01/2.lua
+++ exemplos/exemplo_01/2.lua
@@ -1,9 +1,11 @@
 function handler (evt)
 	event.post {
-        class  = 'ncl',
-        type   = 'presentation',
-        action = 'stop',
-    }
+		class  = 'ncl',
+		type   = 'presentation',
+		action = 'stop',
+		label  = ''
+	}
 end
 
 event.register(handler)
+
--- exemplos/exemplo_01/3.lua
+++ exemplos/exemplo_01/3.lua
@@ -1,11 +1,13 @@
 function handler (evt)
 	event.timer(3000, function()
 		event.post {
-            class  = 'ncl',
-            type   = 'presentation',
-            action = 'stop',
-        }
+			class  = 'ncl',
+			type   = 'presentation',
+			action = 'stop',
+			label  = ''
+		}
 	end)
 end
 
 event.register(handler)
+
